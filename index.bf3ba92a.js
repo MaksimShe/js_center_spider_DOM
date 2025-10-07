c63287fd926b0d64d5c75a9c44403ff1143c449a
SheFing <shevchukm2002@gmail.com>
2025-10-08 00:05:56 +0300

    make build

---
+++ index.bf3ba92a.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".spider"),e=document.querySelector(".wall"),i={height:e.clientHeight/2,width:e.clientWidth/2},h={height:t.clientHeight/2,width:t.clientWidth/2};t.style.top="".concat(i.height-h.height,"px"),t.style.left="".concat(i.width-h.width,"px");
+//# sourceMappingURL=index.bf3ba92a.js.map
