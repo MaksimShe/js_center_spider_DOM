312312f98a55e8eb0d991f54bbf342bf5b31c979
SheFing <shevchukm2002@gmail.com>
2025-10-07 23:49:01 +0300

    att1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+const walls = document.querySelector('.wall');
+
+const wallsCenter = {
+  height: walls.clientHeight / 2,
+  width: walls.clientWidth / 2,
+};
+
+const spiderCenter = {
+  height: spider.clientHeight / 2,
+  width: spider.clientWidth / 2,
+};
+
+spider.style.top = `${wallsCenter.height - spiderCenter.height}px`;
+spider.style.left = `${wallsCenter.width - spiderCenter.width}px`;
